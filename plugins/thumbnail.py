de7c5c712376d633482c663a862891a7dd58a10c
sahaynitin <85211814+sahaynitin@users.noreply.github.com>
2021-11-28 20:35:48 +0530

    Update thumbnail.py

--- plugins/thumbnail.py
+++ plugins/thumbnail.py
@@ -18,7 +18,7 @@ else:
 from PIL import Image
 from pyrogram import filters
 from scripts import Scripted
-from database.db import *
+
 from pyrogram import Client as Clinton
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
