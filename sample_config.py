83e46a3de0b68148cfe52d22234a49c73fdf15d6
sahaynitin <85211814+sahaynitin@users.noreply.github.com>
2021-11-28 20:16:55 +0530

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -34,3 +34,24 @@ class Config(object):
     SESSION_NAME = os.environ.get("SESSION_NAME", "")
     # database uri (mongodb)
     DATABASE_URL = os.environ.get("DATABASE_URL", "")
+    OUO_IO_API_KEY = ""
+
+    MAX_MESSAGE_LENGTH = 4096
+
+    PROCESS_MAX_TIMEOUT = 3600
+    
+    TG_MAX_FILE_SIZE = 2097152000
+
+    DOWNLOAD_LOCATION = "./DOWNLOADS"
+
+    DB_URI = os.environ.get("DATABASE_URL", "")
+
+    UPDATE_GROUP = os.environ.get("UPDATE_GROUP", "")
+    
+    UPDATE_CHANNEL = os.environ.get("UPDATE_CHANNEL", "")
+
+    CHUNK_SIZE = int(os.environ.get("CHUNK_SIZE", 128))
+
+    AUTH_USERS = set(int(x) for x in os.environ.get("AUTH_USERS", "").split())
+
+    BANNED_USERS = set(int(x) for x in os.environ.get("BANNED_USERS", "").split())
